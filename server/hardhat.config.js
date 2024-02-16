230e7695f58a22f0f6c6474b0d626e3ad13f6e8c
Sanjeevvarmabetter <sanjeevvarmacode@gmail.com>
2024-02-16 20:29:34 +0530

    deploy on sepolia

--- server/hardhat.config.js
+++ server/hardhat.config.js
@@ -1,6 +1,14 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config()
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.24",
+  networks: {
+    sepolia: {
+      url: process.env.ALCHEMY_SEPOLIA_URL,
+      account: [process.env.ACCOUNT_PRIVATE_KEY],
+    }
+  }
+
 };
